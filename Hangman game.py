d3384a89ccc57f4e6a25177f54eca484f010c4e8
JKL37 <lifanov.o.1999@mail.ru>
2024-02-15 10:16:18 +0300

    Hangman 1.0

--- Hangman game.py
+++ Hangman game.py
@@ -82,9 +82,8 @@ def get_word():
 
 def play(word):
     print("Давайте сыграем в виселицу!")
-    word_completion = "_" * len(
-        word
-    )  # строка, содержащая символы _ на каждую букву задуманного слова
+    word_completion = "_" * len(word)
+    Flag = False
     guessed = False  # сигнальная метка
     guessed_letters = []  # список уже названных букв
     guessed_words = []  # список уже названных слов
@@ -92,20 +91,65 @@ def play(word):
     print(display_hangman(tries))
     print(word_completion)
 
-    letter = input("Введите букву или слово: ")
-    if len(letter) == 1:
-        pass
+    while Flag == False:
+        letter = input("Введите букву или слово: ")
+
+        if len(letter) == 1:
+            for i in range(len(word)):
+                if letter.upper() == word[i]:
+                    print("Вы угадали букву!")
+                    guessed_letters.append(letter)
+                    word_completion = (
+                        word_completion[:i] + word[i] + word_completion[i + 1 :]
+                    )
+                    print(display_hangman(tries))
+                    print(word_completion)
+                    guessed = True
+                    if word_completion == word:
+                        print("Вы угадали слово! Вы победили! ")
+                        Flag = True
+            if guessed == False:
+                tries -= 1
+                print(display_hangman(tries))
+                print(word_completion)
+                if tries == 0:
+                    print("Вы проиграли!")
+                    break
+                else:
+                    print("Вы не угадали!")
+                    guessed_letters.append(letter)
+
+            guessed = False
+
+        else:
+            if letter.upper() == word:
+                print(display_hangman(tries))
+                print(word)
+                print("Вы угадали слово! Вы победили! ")
+                Flag = True
+            else:
+                tries -= 1
+                print(display_hangman(tries))
+                print(word_completion)
+                if tries == 0:
+                    print("Вы проиграли!")
+                    break
+                else:
+                    print("Вы не угадали!")
+                    guessed_words.append(letter)
 
 
-word_list = ["Виселица", "Угадайка", "Пилот", "Питон", "Корм"]
+word_list = ["ВИСЕЛИЦА", "УГАДАЙКА", "ПИЛОТ", "ПИТОН", "КОРМ"]
+
+rus_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 
 
-Flag = True
-while Flag == True:
+Flag1 = True
+while Flag1 == True:
     word = get_word()
     play(word)
     more = input("Вы хотите сыграть еще? ДА или НЕТ")
     if more == "ДА":
         continue
     elif more == "НЕТ":
-        Flag = False
+        Flag1 = False
